9f3a09bd943eab86e0153210f02b56631360db88
Gopal Katariya <katariyagopal60@gmail.com>
2024-06-08 13:52:27 +0530

    add route constants & pages

---
+++ lib/constants/app_page_routes.dart
@@ -0,0 +1,5 @@
+class AppPageRoute {
+  static const String initialRoute = '/';
+  static const String home = '/';
+  static const String settings = '/settings';
+}
---
+++ lib/utils/app_get_pages.dart
@@ -0,0 +1,12 @@
+import 'package:flutter_template/constants/app_page_routes.dart';
+
+import '../common_imports.dart';
+import '../features/home_page.dart';
+import '../features/settings/pages/settings_page.dart';
+
+class AppGetPages {
+  static List<GetPage<dynamic>> list = [
+    GetPage(name: AppPageRoute.home, page: () => HomePage()),
+    GetPage(name: AppPageRoute.settings, page: () => SettingsPage()),
+  ];
+}
